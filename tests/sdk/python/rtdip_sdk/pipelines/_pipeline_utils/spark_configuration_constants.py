83faff10d4d79d4058dae019b47aba36ea08c6d4
GBBBAS <42962356+GBBBAS@users.noreply.github.com>
2023-03-14 18:48:22 +0000

    Unit Test Fixes
    Signed-off-by: GBBBAS <42962356+GBBBAS@users.noreply.github.com>

--- tests/sdk/python/rtdip_sdk/pipelines/_pipeline_utils/spark_configuration_constants.py
+++ tests/sdk/python/rtdip_sdk/pipelines/_pipeline_utils/spark_configuration_constants.py
@@ -25,11 +25,11 @@ from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.models import Libraries
 from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.constants import DEFAULT_PACKAGES
 
 SPARK_TESTING_CONFIGURATION = {
-    # "spark.executor.cores": "2",
-    # "spark.executor.instances": "1",
-    # "spark.sql.shuffle.partitions": "1",
+    "spark.executor.cores": "2",
+    "spark.executor.instances": "2",
+    "spark.sql.shuffle.partitions": "1",
     "spark.app.name": "test_app", 
-    # "spark.master": "local[*]"
+    "spark.master": "local[*]"
 }
 
 @pytest.fixture(scope="session")
