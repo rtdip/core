0307f377f2655fbac89d0ff4c92c4fc574c40b33
cching95 <73163191+cching95@users.noreply.github.com>
2023-03-15 18:06:51 +0000

    Unit Tests for sources (#88)
    * add pipeline utils unit tests
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * add unit test for eventhub and delta
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * Unit Test Fixes
    
    Signed-off-by: GBBBAS <42962356+GBBBAS@users.noreply.github.com>
    
    * remove test spark
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * add unit test for destination delta
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * fix eventhub spell errors
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    Signed-off-by: GBBBAS <42962356+GBBBAS@users.noreply.github.com>
    Co-authored-by: GBBBAS <42962356+GBBBAS@users.noreply.github.com>

--- src/sdk/python/rtdip_sdk/pipelines/sources/spark/eventhub.py
+++ src/sdk/python/rtdip_sdk/pipelines/sources/spark/eventhub.py
@@ -22,16 +22,16 @@ from ..._pipeline_utils.constants import DEFAULT_PACKAGES
 
 class SparkEventhubSource(SourceInterface):
     '''
-    This Spark source class is used to read batch or streaming data from Event Hubs. Event Hub configurations need to be specified as options in a dictionary.
+    This Spark source class is used to read batch or streaming data from Eventhubs. Eventhub configurations need to be specified as options in a dictionary.
     Additionally, there are more optional configuration which can be found [here.](https://github.com/Azure/azure-event-hubs-spark/blob/master/docs/PySpark/structured-streaming-pyspark.md#event-hubs-configuration){ target="_blank" }
     If using startingPosition or endingPosition make sure to check out **Event Position** section for more details and examples.
     Args:
         spark: Spark Session
-        options: A dictionary of Event Hub configurations (See Attributes table below)
+        options: A dictionary of Eventhub configurations (See Attributes table below)
 
     Attributes:
-        eventhubs.connectionString (str):  Event Hubs connection string is required to connect to the Event Hubs service. (Streaming and Batch)
-        eventhubs.consumerGroup (str): A consumer group is a view of an entire event hub. Consumer groups enable multiple consuming applications to each have a separate view of the event stream, and to read the stream independently at their own pace and with their own offsets. (Streaming and Batch)
+        eventhubs.connectionString (str):  Eventhubs connection string is required to connect to the Eventhubs service. (Streaming and Batch)
+        eventhubs.consumerGroup (str): A consumer group is a view of an entire eventhub. Consumer groups enable multiple consuming applications to each have a separate view of the event stream, and to read the stream independently at their own pace and with their own offsets. (Streaming and Batch)
         eventhubs.startingPosition (JSON str): The starting position for your Structured Streaming job. If a specific EventPosition is not set for a partition using startingPositions, then we use the EventPosition set in startingPosition. If nothing is set in either option, we will begin consuming from the end of the partition. (Streaming and Batch)
         eventhubs.endingPosition: (JSON str): The ending position of a batch query. This works the same as startingPosition. (Batch)
         maxEventsPerTrigger (long): Rate limit on maximum number of events processed per trigger interval. The specified total number of events will be proportionally split across partitions of different volume. (Stream)
@@ -79,7 +79,7 @@ class SparkEventhubSource(SourceInterface):
 
     def read_batch(self) -> DataFrame:
         '''
-        Reads batch data from Event Hubs.
+        Reads batch data from Eventhubs.
         '''
         eventhub_connection_string = "eventhubs.connectionString"
         try:
@@ -101,7 +101,7 @@ class SparkEventhubSource(SourceInterface):
         
     def read_stream(self) -> DataFrame:
         '''
-        Reads streaming data from Event Hubs.
+        Reads streaming data from Eventhubs.
         '''
         eventhub_connection_string = "eventhubs.connectionString"
         try:
---
+++ tests/sdk/python/rtdip_sdk/pipelines/sources/spark/test_delta.py
@@ -0,0 +1,30 @@
+# Copyright 2022 RTDIP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+sys.path.insert(0, '.')
+from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.delta import SparkDeltaDestination
+from src.sdk.python.rtdip_sdk.pipelines.sources.spark.delta import SparkDeltaSource
+from tests.sdk.python.rtdip_sdk.pipelines._pipeline_utils.spark_configuration_constants import spark_session
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+
+def test_spark_delta_read_batch(spark_session: SparkSession):
+    df = spark_session.createDataFrame([{"id": "1"}])
+    delta_destination = SparkDeltaDestination("test_spark_delta_read_batch", {}, "overwrite")       
+    delta_source = SparkDeltaSource(spark_session, {}, "test_spark_delta_read_batch") 
+    delta_destination.write_batch(df)
+    actual_df = delta_source.read_batch()
+    assert isinstance(actual_df, DataFrame)
+    assert actual_df.schema == StructType([StructField('id', StringType(), True)])
