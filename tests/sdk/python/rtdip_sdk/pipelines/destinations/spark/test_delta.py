2e80040160c50442bfdc6b9e987e8141f6fb7b30
JamesKnBr <James.Broady@shell.com>
2023-04-05 15:00:06 +0100

    Added pipeline source/destination setup tests
    Signed-off-by: JamesKnBr <James.Broady@shell.com>

--- tests/sdk/python/rtdip_sdk/pipelines/destinations/spark/test_delta.py
+++ tests/sdk/python/rtdip_sdk/pipelines/destinations/spark/test_delta.py
@@ -16,6 +16,7 @@ import sys
 sys.path.insert(0, '.')
 import pytest
 from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.delta import SparkDeltaDestination
+from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.models import Libraries, MavenLibrary
 from tests.sdk.python.rtdip_sdk.pipelines._pipeline_utils.spark_configuration_constants import spark_session
 from pyspark.sql.functions import lit
 from pyspark.sql import SparkSession
@@ -23,6 +24,18 @@ from pytest_mock import MockerFixture
 
 class TestStreamingQueryClass():
     isActive: bool = False
+
+def test_spark_delta_write_setup():
+    delta_destination = SparkDeltaDestination("test_delta_destination_setup", {}, "overwrite")
+    assert delta_destination.system_type().value == 2
+    assert delta_destination.libraries() == Libraries(maven_libraries=[MavenLibrary(
+                group_id="io.delta",
+                artifact_id="delta-core_2.12",
+                version="2.2.0"
+            )], pypi_libraries=[], pythonwheel_libraries=[])
+    assert isinstance(delta_destination.settings(), dict)
+    assert delta_destination.pre_write_validation()
+    assert delta_destination.post_write_validation()
 
 def test_spark_delta_write_batch(spark_session: SparkSession):
     expected_df = spark_session.createDataFrame([{"id": "1"}])
--- tests/sdk/python/rtdip_sdk/pipelines/sources/spark/test_eventhub.py
+++ tests/sdk/python/rtdip_sdk/pipelines/sources/spark/test_eventhub.py
@@ -16,10 +16,31 @@ import sys
 sys.path.insert(0, '.')
 import pytest
 from src.sdk.python.rtdip_sdk.pipelines.sources.spark.eventhub import SparkEventhubSource
+from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.models import Libraries, MavenLibrary
 from tests.sdk.python.rtdip_sdk.pipelines._pipeline_utils.spark_configuration_constants import spark_session
 import json
 from pyspark.sql import DataFrame, SparkSession
 from pytest_mock import MockerFixture
+def test_spark_eventhub_read_setup(spark_session: SparkSession):
+
+    connection_string = "Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=test;EntityPath=test"
+    eventhub_configuration = {
+        "eventhubs.connectionString": connection_string, 
+        "eventhubs.consumerGroup": "$Default",
+        "eventhubs.startingPosition": json.dumps({"offset": "0", "seqNo": -1, "enqueuedTime": None, "isInclusive": True})
+    }
+    eventhub_source = SparkEventhubSource(spark_session, eventhub_configuration)
+    assert eventhub_source.system_type().value == 2
+    assert eventhub_source.libraries() == Libraries(maven_libraries=[MavenLibrary(
+                group_id="com.microsoft.azure", 
+                artifact_id="azure-eventhubs-spark_2.12",
+                version="2.3.22"
+            )], pypi_libraries=[], pythonwheel_libraries=[])
+    assert isinstance(eventhub_source.settings(), dict)
+    assert eventhub_source.pre_read_validation()
+    with pytest.raises(Exception):
+        eventhub_source.post_read_validation(spark_session.createDataFrame([{"a": "x"}]))
+
 
 def test_spark_eventhub_read_batch(spark_session: SparkSession):
     connection_string = "Endpoint=sb://test.servicebus.windows.net/;SharedAccessKeyName=test;SharedAccessKey=test;EntityPath=test"
