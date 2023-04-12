0ac8edb8f477636104afefc86bddfbc057eb46b8
rodalynbarce <Rodalyn.Barce@shell.com>
2023-04-12 14:25:12 +0100

    Added pipeline source IoT Hub and test
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>

--- src/sdk/python/rtdip_sdk/pipelines/sources/spark/iot_hub.py
+++ src/sdk/python/rtdip_sdk/pipelines/sources/spark/iot_hub.py
@@ -20,9 +20,22 @@ from ..interfaces import SourceInterface
 from ..._pipeline_utils.models import Libraries, SystemType
 from ..._pipeline_utils.constants import DEFAULT_PACKAGES, EVENTHUB_SCHEMA
 
-class SparkEventhubSource(SourceInterface):
+class SparkIoThubSource(SourceInterface):
     '''
-    This Spark source class is used to read batch or streaming data from an IoT Hub. 
+    This Spark source class is used to read batch or streaming data from an IoT Hub. IoT Hub configurations need to be specified as options in a dictionary.
+    Additionally, there are more optional configurations which can be found [here.](https://github.com/Azure/azure-event-hubs-spark/blob/master/docs/PySpark/structured-streaming-pyspark.md#event-hubs-configuration){ target="_blank" }
+    If using startingPosition or endingPosition make sure to check out the **Event Position** section for more details and examples.
+    Args:
+        spark: Spark Session
+        options: A dictionary of IoT Hub configurations (See Attributes table below)
+
+    Attributes:
+        eventhubs.connectionString (str):  IoT Hub connection string is required to connect to the Eventhubs service. (Streaming and Batch)
+        eventhubs.consumerGroup (str): A consumer group is a view of an entire IoT Hub. Consumer groups enable multiple consuming applications to each have a separate view of the event stream, and to read the stream independently at their own pace and with their own offsets. (Streaming and Batch)
+        eventhubs.startingPosition (JSON str): The starting position for your Structured Streaming job. If a specific EventPosition is not set for a partition using startingPositions, then we use the EventPosition set in startingPosition. If nothing is set in either option, we will begin consuming from the end of the partition. (Streaming and Batch)
+        eventhubs.endingPosition: (JSON str): The ending position of a batch query. This works the same as startingPosition. (Batch)
+        maxEventsPerTrigger (long): Rate limit on maximum number of events processed per trigger interval. The specified total number of events will be proportionally split across partitions of different volume. (Stream)
+    
     '''
     spark: SparkSession
     options: dict
@@ -62,11 +75,11 @@ class SparkEventhubSource(SourceInterface):
         '''
         Reads batch data from IoT Hubs.
         '''
-        eventhub_connection_string = "eventhubs.connectionString"
+        iothub_connection_string = "eventhubs.connectionString"
         try:
-            if eventhub_connection_string in self.options:
+            if iothub_connection_string in self.options:
                 sc = self.spark.sparkContext
-                self.options[eventhub_connection_string] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(self.options[eventhub_connection_string])
+                self.options[iothub_connection_string] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(self.options[iothub_connection_string])
 
             return (self.spark
                 .read
@@ -88,9 +101,9 @@ class SparkEventhubSource(SourceInterface):
         '''
         iothub_connection_string = "eventhubs.connectionString"
         try:
-            if eventhub_connection_string in self.options:
+            if iothub_connection_string in self.options:
                 sc = self.spark.sparkContext
-                self.options[eventhub_connection_string] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(self.options[eventhub_connection_string])
+                self.options[iothub_connection_string] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(self.options[iothub_connection_string])
 
             return (self.spark
                 .readStream
