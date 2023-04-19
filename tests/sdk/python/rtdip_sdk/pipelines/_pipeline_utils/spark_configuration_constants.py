3b15fb4b45d38407e3981080c6660ef6d374b3aa
cching95 <73163191+cching95@users.noreply.github.com>
2023-04-19 17:18:40 +0100

    Spark Source and Destination for Kafka (#158)
    * add kafka source and destination
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * updates for unit tests
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * add unit tests for kafka source and destination
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * latest updates
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * fix code reference docs
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>

--- tests/sdk/python/rtdip_sdk/pipelines/_pipeline_utils/spark_configuration_constants.py
+++ tests/sdk/python/rtdip_sdk/pipelines/_pipeline_utils/spark_configuration_constants.py
@@ -24,10 +24,12 @@ from unittest.mock import patch
 from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.delta import SparkDeltaDestination
 from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.kinesis import SparkKinesisDestination
 from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.eventhub import SparkEventhubDestination
+from src.sdk.python.rtdip_sdk.pipelines.destinations.spark.kafka import SparkKafkaDestination
 from src.sdk.python.rtdip_sdk.pipelines.sources.spark.delta import SparkDeltaSource
 from src.sdk.python.rtdip_sdk.pipelines.sources.spark.kinesis import SparkKinesisSource
 from src.sdk.python.rtdip_sdk.pipelines.sources.spark.delta_sharing import SparkDeltaSharingSource
 from src.sdk.python.rtdip_sdk.pipelines.sources.spark.eventhub import SparkEventhubSource
+from src.sdk.python.rtdip_sdk.pipelines.sources.spark.kafka import SparkKafkaSource
 from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.spark import SparkClient
 from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.models import Libraries
 from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.constants import DEFAULT_PACKAGES
@@ -42,7 +44,7 @@ SPARK_TESTING_CONFIGURATION = {
 
 @pytest.fixture(scope="session")
 def spark_session():
-    component_list = [SparkDeltaSource(None, {}, "test_table"), SparkDeltaSharingSource(None, {}, "test_table"), SparkDeltaDestination("test_table", {}), SparkEventhubSource(None, {}), SparkEventhubDestination({}), SparkKinesisSource(None, {}), SparkKinesisDestination({})]
+    component_list = [SparkDeltaSource(None, {}, "test_table"), SparkDeltaSharingSource(None, {}, "test_table"), SparkDeltaDestination("test_table", {}), SparkEventhubSource(None, {}), SparkEventhubDestination({}),  SparkKafkaSource(None, {}), SparkKafkaDestination({}), SparkKinesisSource(None, {}), SparkKinesisDestination({})]
     task_libraries = Libraries()
     task_libraries.get_libraries_from_components(component_list)
     spark_configuration = SPARK_TESTING_CONFIGURATION.copy()
