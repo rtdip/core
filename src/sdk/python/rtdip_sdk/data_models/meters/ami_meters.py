87e54f7ab9ceaecb91ee6b03f1f8d708cc72b1c4
Victor Bayon <victor@innowatts.com>
2023-04-25 20:45:08 +0100

    Added docstrings to models

--- src/sdk/python/rtdip_sdk/data_models/meters/ami_meters.py
+++ src/sdk/python/rtdip_sdk/data_models/meters/ami_meters.py
@@ -23,40 +23,115 @@ class ModelType(IntFlag):
 
 
 class UomUsage(Enum):
-    w = 0
+    """
+    Units of measurement
+    """
+    w = 0 
+    """Watts"""
     wh = 1
+    """Watts/Hour"""
     kw = 2
+    """Kilowatts"""
     kwh = 3
+    """Kilowatts/Hour"""
     mw = 4
+    """Megawatts"""
     mwh = 5
-    # Other
+    """Megawatts/Hour"""
 
 
 class SeriesType(IntFlag):
-    # Keep the order when refactoring.
+    """
+    Definition of the type of timeseries for the measurements (e.g. realtime or interval based) and the type of the computation if the series is aggregated/derived 
+    """
     real_time = auto()
+    """
+    The data has no specific time pattern
+    """
     minute_1 = auto()
+    """
+    1 minute interval
+    """
     minutes_5 = auto()
+    """
+    5 minutes interval 
+    """
     minutes_10 = auto()
+    """
+    10 minutes interval 
+    """
     minutes_15 = auto()
+    """
+    15 minutes interval 
+    """
     minutes_30 = auto()
+    """
+    30 minutes interval
+    """
     minutes_60 = auto()
-    minutes_2_hours = auto()
-    minutes_3_hours = auto()
-    minutes_4_hours = auto()
-    minutes_6_hours = auto()
-    minutes_8_hours = auto()
-    minutes_12_hours = auto()
-    minutes_24_hours = auto()
-    hour = auto()
-    day = auto()
+    """
+    60 minutes interval
+    """
+    hours_2 = auto()
+    """
+    2 hours interval
+    """
+    hours_3 = auto()
+    """
+    3 hours interval
+    """
+    hours_4 = auto()
+    """
+    4 hours interval
+    """
+    hours_5 = auto()
+    """
+    5 hours interval
+    """
+    hours_6 = auto()
+    """
+    6 hours interval
+    """
+    hours_8 = auto()
+    """
+    8 hours interval
+    """
+    hours_12 = auto()
+    """
+    12 hours interval
+    """
+    hours_24 = auto()
+    """
+    1 Minute interval
+    """
     week = auto()
+    """
+    1 Minute interval
+    """
     month = auto()
+    """
+    1 Minute interval
+    """
     year = auto()
-    # Computations
+    """
+    1 Minute interval
+    """
     sum = auto()
-    average_filter = auto()
+    """
+    Measurement is the result of computing the sum of a set of measurements
+    """
+    mean_filter = auto()
+    """
+    Measurement is the result of computing the mean of a set of measurements
+    """
+    median_filter = auto()
+    """
+    Measurement is the result of computing the median of a set of measurements
+    """
     max_filter = auto()
+    """
+    Measurement is the result of computing the max of a set of measurements
+    """
     min_filter = auto()
     # Testing
     test = auto()
@@ -65,45 +140,145 @@ class SeriesType(IntFlag):
 
 
 class Usage(BaseModel):
-    uid: str  # A unique identifier tied to a timeseries of data.
-    series_id: str  # Identifier for a particular timeseries set
-    timestamp: int  # Creation time. Always UTC. Seconds since EPOCH
-    interval_timestamp: int  # The timestamp for the interval. Always UTC. Seconds since EPOCH.
-    value: float  # The actual value of the measurement
+    """
+    Usage. a usage measurement from an AMI meter 
+    """
+    uid: str  
+    """
+    A unique identifier associated to the source of the measurement (e.g. sensor, meter, etc.)
+    """
+    series_id: str
+    """
+    Identifier for a particular timeseries set
+    """
+    timestamp: int
+    """
+    Creation time. Always UTC. Seconds since EPOCH
+    """
+    interval_timestamp: int
+    """
+    The timestamp for the interval. Always UTC. Seconds since EPOCH
+    """
+    value: float
+    """
+    The actual value of the measurement
+    """
 
 
 
 class ValueType(IntFlag):
-    # Keep the order when refactoring.
+    """
+    Defines the type of value
+    """
     counter = auto()
+    """
+    The value is cumulative increasing monotinically
+    """   
     gauge = auto()
+    """
+    The value can arbitrarily go up and down
+    """
     histogram = auto()
+    """
+    The value is a histogram
+    """
     summary = auto()
+    """
+    """
     usage = auto()
+    """
+    The value is from source that consumes energy
+    """
     generation = auto()
+    """
+    The value is from a source that generates energy
+    """
     prediction = auto()
+    """
+    The value is generated from a predictive model
+    """
     short_term = auto()
+    """
+    The value is related to a short term (e.g. short term forecast)
+    """
     long_term = auto()
+    """
+    The value is related to a long term (e.g. long term forecast)
+    """
+    actuals = auto()
+    """
+    The value is from a actual measurement
+    """
     backcast = auto()
+    """
+    The value is related to a forecast that happens in the past (e.g. for calculating how good was the forecast compared to actuals)
+    """
     forecast = auto()
+    """
+    The value is related to a forecast in the future
+    """
     short_term_backcast = short_term | backcast
     long_term_term_backcast = long_term | backcast
     short_term_forecast = short_term | forecast
     long_term_term_forecast = long_term | forecast
 
 class MetaData(BaseModel):
-    uid: str  # Unique identifier (e.g. sensor, meter, etc)
-    series_id: str  # Unique Identifier for a particular time series (TS)
-    series_parent_id: str  # Hierarchy (Sequence) of this TS associated to the same group of TS
-    name: str  # Name of the sensor
-    uom: UomUsage  # Unit of measure for this sensor
-    description: str  # Short description
-    timestamp_start: int  # Timestamp of the creation of the record and start of the timeseries. UTC
-    timestamp_end: int  # Timestamp of end of the timeseries. UTC
-    time_zone: str  # Time zone of where the sensor or where the series has started
-    version: str  # Version of this uid/series_id
-    series_type: SeriesType  # Type of time series (e.g. pure time series, interval type, etc.)
-    model_type: ModelType  # Type for this series (sensor type, measurement type, forecast type, etc)
+    """
+    Metadata for a sensor, meter, etc. and its association to sets of time series
+    """
+    uid: str
+    """
+    A unique identifier associated to the source of the measurement (e.g. sensor, meter, etc.)
+    """
+    series_id: str
+    """
+    Identifier for a particular timeseries set
+    """
+    series_parent_id: str
+    """
+    Hierarchy (Sequence) of this TS associated to the same group of TS
+    """
+    name: str
+    """
+    Name of the sensor
+    """
+    uom: UomUsage 
+    """
+    Unit of measure for this sensor
+    """
+    description: str
+    """
+    Short description
+    """
+    timestamp_start: int
+    """
+    Timestamp of the creation of the record and start of the timeseries.  Always UTC. Seconds since EPOCH
+    """
+    timestamp_end: int 
+    """
+    Timestamp of end of the timeseries.  Always UTC. Seconds since EPOCH
+    """
+    time_zone: str
+    """
+    Time zone of where the sensor or where the series has started
+    """
+    version: str 
+    """
+    For versioning
+    """
+    series_type: SeriesType 
+    """
+    Type of the timeseries
+    """
+    model_type: ModelType
+    """
+    Type of model use to produce this data (e.g. a precitive model)
+    """
     value_type: ValueType
-    # for this time_series id
-    properties: dict  # Any Additional properties (Key/Value)
+    """
+    Type of value of the timeseries
+    """
+    properties: dict  
+    """
+    Any other additional properties (Key/Value)
+    """
--- src/sdk/python/rtdip_sdk/data_models/weather/weather_models.py
+++ src/sdk/python/rtdip_sdk/data_models/weather/weather_models.py
@@ -18,88 +18,88 @@ from datetime import datetime
 
 class AtmosphericG215minForecastV1(BaseModel):
     """
-    Weather Model describing the Atmospheric G2 Forecast at 15 minutes intervals.
+    The Hourly Forecast API is sourced from the The Weather Company Forecast system. 
     """
     clas: str  
-    """ Data identifier: example: fod_long_range_hourly"""
+    """Data identifier. Example: fod_long_range_hourly"""
     clds: int  
-    """Cloud Cover: Hourly average cloud cover expressed as a percentage: range: 0 to 100"""
+    """Cloud Cover: Hourly average cloud cover expressed as a percentage. Range: 0 to 100"""
     day_ind: str  
-    """DayOrNight: range: D, N, X   X=Missing"""
+    """This data field indicates whether it is daytime or nighttime based on the Local Apparent Time of the location. Range: D, N, X   X=Missing"""
     dewpt: int  
-    """Dew Point: example: 63"""
+    """Dew Point. The temperature which air must be cooled at constant pressure to reach saturation. The Dew Point is also an indirect measure of the humidity of the air. The Dew Point will never exceed the Temperature. When the Dew Point and Temperature are equal, clouds or fog will typically form. The closer the values of Temperature and Dew Point, the higher the relative humidity"""
     dow: str  
-    """dayOfWeek: range: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"""
+    """Day of week. Range: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"""
     expire_time_gmt: float  
-    """expirationTimeUtc [epoch] Expiration time in UNIX seconds: example: 1369252800"""
+    """Expiration time in UNIX seconds."""
     fcst_valid: float  
-    """Time forecast is valid in UNIX seconds [epoch]: example: 1369306800"""
+    """Time forecast is valid in UNIX seconds."""
     fcst_valid_local: datetime  
-    """Time forecast is valid in local apparent time [ISO]: example: 2013-08-06T07:00:00-0400"""
+    """Time forecast is valid in local apparent time [ISO]"""
     feels_like: int  
-    """example: 84"""
+    """Hourly feels like temperature. An apparent temperature. It represents what the air temperature “feels like” on exposed human skin due to the combined effect of the wind chill or heat index"""
     golf_category: str  
-    """example: very good"""
+    """The Golf Index Category expressed as a worded phrase the weather conditions for playing golf"""
     golf_index: int  
-    """range: 1-10: enum: 0-2=Very Poor, 3=Poor, 4-5=Fair, 6-7=Good, 8-9=Very Good, 10=Excellent"""
+    """The Golf Index expresses on a scale of 0 to 10 the weather conditions for playing golf. Not applicable at night. Enum: 0-2=Very Poor, 3=Poor, 4-5=Fair, 6-7=Good, 8-9=Very Good, 10=Excellent"""
     gust: int
-    """gust """
+    """The maximum expected wind gust speed"""
     hi: int  
     """Hourly maximum heat index"""
     icon_code: int
-    """example: 26"""
+    """This number is the key to the weather icon lookup. The data field shows the icon number that is matched to represent the observed weather conditions. Please refer to the Forecast Icon Code, Weather Phrases and Images document"""
     icon_extd: int  
-    """example 5560"""
+    """Code representing explicit full set sensible weather"""
     mslp: float 
     """Hourly mean sea level pressure"""
-    num: int  # 
-    """range: 1-15"""
+    num: int  
+    """This data field is the sequential number that identifies each of the forecasted days in the API. They start on day 1, which is the forecast for the current day. Then the forecast for tomorrow uses number 2, then number 3 for the day after tomorrow, and so forth. Range: 1-15"""
     phrase_12char: str
-    """phrase_12char"""
+    """Hourly sensible weather phrase"""
     phrase_22char: str
-    """phrase_22char"""
+    """Hourly sensible weather phrase"""
     phrase_32char: str
-    """phrase_32char"""
+    """Hourly sensible weather phrase"""
     pop: str 
-    """Hourly maximum probability of precipitation: range 0-100"""
+    """Hourly maximum probability of precipitation.  Range 0-100"""
     precip_type: str  
-    """enum: rain,snow, precip"""
+    """The short text describing the expected type accumulation associated with the Probability of Precipitation (POP) display for the hou. Enum: rain,snow, precip"""
     qpf: float  
-    """qpf"""
+    """The forecasted measurable precipitation (liquid or liquid equivalent) during the hour"""
     rh: int  
-    """enum 0-100"""
+    """The relative humidity of the aire. Range 0-100"""
     severity: int  
-    """severity example: 0 = no threat 6 = dangerous / life threatening"""
+    """A number denoting how impactful is the forecasted weather for this hour. Range: 0 = no threat,  6 = dangerous / life threatening"""
     snow_qpf: float 
-    """snow_qpf"""
+    """The forecasted hourly snow accumulation during the hour"""
     subphrase_pt1: str 
-    """subphrase_pt1"""
+    """Part 1 of 3-part hourly sensible weather phrase"""
     subphrase_pt2: str 
-    """subphrase_pt2"""
+    """Part 2 of 3-part hourly sensible weather phrase"""
     subphrase_pt3: str
-    """subphrase_pt3"""
+    """Part 3 of 3-part hourly sensible weather phrase"""
     temp: int  
-    """range -140 to 140"""
+    """The temperature of the air, measured by a thermometer 1.5 meters (4.5 feet) above the ground that is shaded from the other element"""
     uv_desc: str  
-    """enum: -2 is Not Available-1 is No Report 0 to 2 is Low 3 to 5 is Moderate 6 to 7 is High 8 to 10 is Very High 11 to 16 is Extreme"""
+    """The UV Index Description which complements the UV Index value by providing an associated level of risk of skin damage due to exposur. Eenum: -2 is Not Available-1 is No Report 0 to 2 is Low 3 to 5 is Moderate 6 to 7 is High 8 to 10 is Very High 11 to 16 is Extreme"""
     uv_index: int 
-    """uv_index"""
+    """Hourly maximum UV index"""
     uv_index_raw: float 
-    """uv_index_raw"""
+    """The non-truncated UV Index which is the intensity of the solar radiation based on a number of factors"""
     uv_warning: int 
-    """uv_warning"""
+    """TWC-created UV warning based on UV index of 11 or greater"""
     vis: float  
-    """range 0 - 999""" 
+    """Prevailing hourly visibility""" 
     wc: int 
-    """wc"""
+    """Hourly minimum wind chill"""
     wdir: int  
-    """range 0 - 359"""
+    """Hourly average wind direction in magnetic notation. Range 0 - 359"""
     wdir_cardinal: str  
-    """enum: N , NNE , NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW, CALM, VAR"""
+    """Hourly average wind direction in cardinal notation. Enum: N , NNE , NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW, CALM, VAR"""
     wspd: int 
-    """wspd"""
+    """The maximum forecasted hourly wind speed"""
     wxman: str 
-    """wxman"""
+    """Code combining Hourly sensible weather and temperature conditions"""
     
 
 
