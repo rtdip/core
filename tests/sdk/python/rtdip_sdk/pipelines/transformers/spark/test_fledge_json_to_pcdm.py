9fe4f6a798e479430e38547119a3f34b93d607cc
rodalynbarce <Rodalyn.Barce@shell.com>
2023-05-04 12:52:38 +0100

    added test
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>

--- tests/sdk/python/rtdip_sdk/pipelines/transformers/spark/test_fledge_json_to_pcdm.py
+++ tests/sdk/python/rtdip_sdk/pipelines/transformers/spark/test_fledge_json_to_pcdm.py
@@ -12,7 +12,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pyspark.sql import SparkSession, DataFrame
+import sys
+sys.path.insert(0, '.')
+from src.sdk.python.rtdip_sdk.pipelines.transformers.spark.fledge_json_to_pcdm import FledgeJsonToPCDMTransformer
+from src.sdk.python.rtdip_sdk.pipelines._pipeline_utils.models import Libraries, SystemType
+from tests.sdk.python.rtdip_sdk.pipelines._pipeline_utils.spark_configuration_constants import spark_session
 
-def test_fledge_json_to_pcdm(sparksesion: SparkSession):
-    fledge_json_data = '[{"asset":"testAsset","readings":{"testTag":-0.913545458},"timestamp":"2023-05-03 08:45:42.509118+00:00"}]'
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from datetime import datetime
+
+def test_fledge_json_to_pcdm(spark_session: SparkSession):
+    fledge_json_data = '[{"asset":"testAsset1","readings":{"testTag1":-0.913545458},"timestamp":"2023-05-03 08:45:42.509118+00:00"}, {"asset":"testAsset2","readings":{"testTag2":-0.913545458},"timestamp":"2023-05-04 08:45:42.509118+00:00"}]'
+    fledge_df: DataFrame = spark_session.createDataFrame([{"body": fledge_json_data}])
+
+    expected_schema = StructType([
+        StructField("TagName", StringType(), False),
+        StructField("Value", StringType(), True),
+        StructField("EventTime", TimestampType(), True),
+        StructField("Status", StringType(), False),
+        StructField("DataType", StringType(), True),
+    ])
+
+    expected_data = [
+        {"TagName":"testTag1", "Value":"-0.913545458", "EventTime": datetime.fromisoformat("2023-05-03 08:45:42.509118+00:00"), "Status":"good", "DataType":"float"}, 
+        {"TagName":"testTag2", "Value":"-0.913545458", "EventTime": datetime.fromisoformat("2023-05-04 08:45:42.509118+00:00"), "Status":"good", "DataType":"float"}, 
+    ]
+
+    expected_df: DataFrame = spark_session.createDataFrame(
+        schema=expected_schema,
+        data=expected_data
+    )
+
+    eventhub_json_to_fledge_transformer = FledgeJsonToPCDMTransformer(fledge_df)
+    actual_df = eventhub_json_to_fledge_transformer.transform()
+
+    assert eventhub_json_to_fledge_transformer.system_type() == SystemType.PYSPARK
+    assert isinstance(eventhub_json_to_fledge_transformer.libraries(), Libraries)
+    assert expected_schema == actual_df.schema
+    assert expected_df.collect() == actual_df.collect()
