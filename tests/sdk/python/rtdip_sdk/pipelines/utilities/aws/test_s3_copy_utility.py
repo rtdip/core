257259e6cf2e54f2eb5b2d11f7c81cce22b022db
Victor Bayon <victor@innowatts.com>
2023-07-10 11:55:45 +0100

    Refactored imports

--- tests/sdk/python/rtdip_sdk/pipelines/utilities/aws/test_s3_copy_utility.py
+++ tests/sdk/python/rtdip_sdk/pipelines/utilities/aws/test_s3_copy_utility.py
@@ -1,15 +1,31 @@
-import boto3
-import sys
-import random
-import string
-from moto import mock_s3
-import tempfile
+# Copyright 2022 RTDIP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
+import tempfile
+import string
+import random
+import sys
+import boto3
+from moto import mock_s3
 
 
-from  src.sdk.python.rtdip_sdk.pipelines.utilities.aws.s3_copy_utility import S3CopyUtility
+sys.path.insert(0, '.')
+
+from src.sdk.python.rtdip_sdk.pipelines.utilities.aws.s3_copy_utility import S3CopyUtility
 from src.sdk.python.rtdip_sdk.data_models.storage_objects import utils
+
 
 @mock_s3
 def test_s3_copy_utility():
@@ -54,9 +70,6 @@ def test_s3_copy_utility():
     result: bool = s3_copy_utility.execute()
     assert(result is True)
 
-
-
-
     # Download from S3
     # Calculate the hash of the file, delete it and download it from S3 and re-compute the hash and check if it is the same
     os.remove(rnd_tempfile.name)
@@ -65,7 +78,7 @@ def test_s3_copy_utility():
     assert(result is True)
     assert(os.path.exists(rnd_tempfile.name is True))
 
-    
+
 
 
     
