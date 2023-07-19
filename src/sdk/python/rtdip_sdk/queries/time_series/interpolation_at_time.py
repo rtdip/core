a9535fb6bf3d467a8a682f419b3abd8d84283286
cching95 <73163191+cching95@users.noreply.github.com>
2023-07-19 17:41:40 +0100

    Remove JinjaSQL and refactor query builder for raw and metadata functions (#384)
    * refactor query builder to remove jinjasql
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * update parameters for query builder
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    * updated unit tests for raw and metadata
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>

--- src/sdk/python/rtdip_sdk/queries/time_series/interpolation_at_time.py
+++ src/sdk/python/rtdip_sdk/queries/time_series/interpolation_at_time.py
@@ -47,7 +47,7 @@ def get(connection: object, parameters_dict: dict) -> pd.DataFrame:
         raise ValueError("timestamps must be a list")
 
     try:
-        query = _query_builder(parameters_dict, metadata=False, interpolation_at_time=True)
+        query = _query_builder(parameters_dict, "interpolation_at_time", interpolation_at_time=True)
 
         try:
             cursor = connection.cursor()
