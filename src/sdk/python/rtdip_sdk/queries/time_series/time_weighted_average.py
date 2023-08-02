4d5a4dac9f970476bb9e43970dfb68b1bf2338d8
cching95 <73163191+cching95@users.noreply.github.com>
2023-08-02 12:45:42 +0100

    Update Unit Tests for new metadata Step
    Signed-off-by: cching95 <73163191+cching95@users.noreply.github.com>

--- src/sdk/python/rtdip_sdk/queries/time_series/time_weighted_average.py
+++ src/sdk/python/rtdip_sdk/queries/time_series/time_weighted_average.py
@@ -13,10 +13,7 @@
 # limitations under the License.
 import logging
 import pandas as pd
-import sys
-sys.path.insert(0, '.')
-from src.sdk.python.rtdip_sdk.queries.time_series._query_builder import _query_builder
-
+from ._query_builder import _query_builder
 
 def get(connection: object, parameters_dict: dict) -> pd.DataFrame:
     '''
@@ -43,7 +40,7 @@ def get(connection: object, parameters_dict: dict) -> pd.DataFrame:
         time_interval_unit (str): The time interval unit (second, minute, day, hour)
         window_length (int): Add longer window time in days for the start or end of specified date to cater for edge cases.
         include_bad_data (bool): Include "Bad" data points with True or remove "Bad" data points with False
-        step (str): data points with step "enabled" or "disabled". The options for step are "metadata", "true" or "false". "metadata" will get the step requirements from the metadata table if applicable.
+        step (str): data points with step "enabled" or "disabled". The options for step are "true", "false" or "metadata". "metadata" will retrieve the step value from the metadata table.
     Returns:
         DataFrame: A dataframe containing the time weighted averages.
     '''
@@ -71,26 +68,3 @@ def get(connection: object, parameters_dict: dict) -> pd.DataFrame:
     except Exception as e:
         logging.exception('error with time weighted average function')
         raise e
-    
-from src.sdk.python.rtdip_sdk.authentication.azure import DefaultAuth
-from src.sdk.python.rtdip_sdk.connectors import DatabricksSQLConnection
-#testing 
-auth = DefaultAuth(exclude_visual_studio_code_credential=True).authenticate()
-token = auth.get_token("2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default").token
-connection = DatabricksSQLConnection("adb-8969364155430721.1.azuredatabricks.net", "/sql/1.0/endpoints/9ecb6a8d6707260c", token)
-parameters = {
-    "business_unit": "downstream",
-    "region": "EMEA",
-    "asset": "pernis",
-    "data_security_level": "restricted",
-    "data_type": "float",
-    "tag_names": ["SRU:050QR012.PV", "PGP:000XX102.PV"],
-    "start_date": "2023-01-01T23:00:00+0000",
-    "end_date": "2023-01-02T06:00:00+0000",
-    "window_size_mins": 15, 
-    "step" : "false",
-    "window_length": 1, 
-    "include_bad_data": False, #options: [True, False]
-}
-x = get(connection, parameters)
-print(x)
