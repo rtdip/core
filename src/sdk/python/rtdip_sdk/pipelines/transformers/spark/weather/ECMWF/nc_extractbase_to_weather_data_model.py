e851acb863dfae8fdd819e954eaea67d54868606
Amber-Rigg <amber.l.rigg25@gmail.com>
2023-08-21 13:44:36 +0100

    Documentation improvement
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- src/sdk/python/rtdip_sdk/pipelines/transformers/spark/weather/ECMWF/nc_extractbase_to_weather_data_model.py
+++ src/sdk/python/rtdip_sdk/pipelines/transformers/spark/weather/ECMWF/nc_extractbase_to_weather_data_model.py
@@ -35,8 +35,8 @@ class ECMWFExtractBaseToWeatherDataModel(TransformerInterface):
         date_end (str): End date of extraction in "YYYY-MM-DD HH:MM:SS" format
         run_frequency (str):Frequency format of runs to download, e.g. "H"
         run_interval (str): Interval of runs, e.g. a run_frequency of "H" and run_interval of "12" will extract the data of the 00 and 12 run for each day.
-        lat (xr.DataArray) : Latitude values to extract from nc files
-        lon (xr.DataArray) : Longitude values to extract from nc files
+        lat (DataArray): Latitude values to extract from nc files
+        lon (DataArray): Longitude values to extract from nc files
         utc (bool = True): Whether to convert the time to UTC or not
     """
 
