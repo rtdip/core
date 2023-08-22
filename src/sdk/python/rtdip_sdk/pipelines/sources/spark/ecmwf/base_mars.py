f4f78161ed15985cae08b27f201f2204b1ea59f3
Amber-Rigg <amber.l.rigg25@gmail.com>
2023-08-22 12:47:36 +0100

    Correct Naming
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- src/sdk/python/rtdip_sdk/pipelines/sources/spark/ecmwf/base_mars.py
+++ src/sdk/python/rtdip_sdk/pipelines/sources/spark/ecmwf/base_mars.py
@@ -30,8 +30,8 @@ class SparkECMWFBaseMarsSource:
         date_start (str): Start date of extraction in "YYYY-MM-DD HH:MM:SS" format
         date_end (str): End date of extraction in "YYYY-MM-DD HH:MM:SS" format
         ecmwf_api_key (str): API key for ECMWF MARS server
-        ecwmf_api_url (str): URL for ECMWF MARS server
         ecmwf_api_email (str): Email for ECMWF MARS server
+        ecmwf_api_url (str): URL for ECMWF MARS server
         run_frequency (str):Frequency format of runs to download, e.g. "H"
         run_interval (str): Interval of runs, e.g. a run_frequency of "H" and run_interval of "12" will extract the data of the 00 and 12 run for each day.
     """
