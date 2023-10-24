f80733af82a832c9ba10d409114e0f58afca4c51
Chloe Ching <chloe.ching@shell.com>
2023-10-24 17:07:22 +0100

    update example for delta table create
    Signed-off-by: Chloe Ching <chloe.ching@shell.com>

--- src/sdk/python/rtdip_sdk/pipelines/utilities/spark/delta_table_create.py
+++ src/sdk/python/rtdip_sdk/pipelines/utilities/spark/delta_table_create.py
@@ -36,7 +36,30 @@ class DeltaTableCreateUtility(UtilitiesInterface):
     """
     Creates a Delta Table in a Hive Metastore or in Databricks Unity Catalog.
 
-    Args:
+    Example
+    -------
+    ```python
+    from rtdip_sdk.pipelines.utilities.spark.delta_table_create import DeltaTableCreateUtility, DeltaTableColumn
+
+    table_create_utility = DeltaTableCreateUtility(
+        spark=spark_session,
+        table_name="delta_table",
+        columns=[
+            DeltaTableColumn(name="EventDate", type="date", nullable=False, metadata={"delta.generationExpression": "CAST(EventTime AS DATE)"}),
+            DeltaTableColumn(name="TagName", type="string", nullable=False),
+            DeltaTableColumn(name="EventTime", type="timestamp", nullable=False),
+            DeltaTableColumn(name="Status", type="string", nullable=True),
+            DeltaTableColumn(name="Value", type="float", nullable=True)
+        ],
+        partitioned_by=["EventDate"],
+        properties={"delta.logRetentionDuration": "7 days", "delta.enableChangeDataFeed": "true"},
+        comment="Creation of Delta Table"
+    )
+
+    result = table_create_utility.execute()
+    ```
+
+    Parameters:
         spark (SparkSession): Spark Session required to read data from cloud storage
         table_name (str): Name of the table, including catalog and schema if table is to be created in Unity Catalog
         columns (list[DeltaTableColumn]): List of columns and their related column properties
@@ -45,27 +68,7 @@ class DeltaTableCreateUtility(UtilitiesInterface):
         properties (dict, optional): Propoerties that can be specified for a Delta Table. Further information on the options available are [here](https://docs.databricks.com/delta/table-properties.html#delta-table-properties)
         comment (str, optional): Provides a comment on the table metadata
 
-    Example:
-        ```python
-        from rtdip_sdk.pipelines.utilities.spark.delta_table_create import DeltaTableCreateUtility, DeltaTableColumn
 
-        table_create_utility = DeltaTableCreateUtility(
-            spark=spark_session,
-            table_name="delta_table",
-            columns=[
-                DeltaTableColumn(name="EventDate", type="date", nullable=False, metadata={"delta.generationExpression": "CAST(EventTime AS DATE)"}),
-                DeltaTableColumn(name="TagName", type="string", nullable=False),
-                DeltaTableColumn(name="EventTime", type="timestamp", nullable=False),
-                DeltaTableColumn(name="Status", type="string", nullable=True),
-                DeltaTableColumn(name="Value", type="float", nullable=True)
-            ],
-            partitioned_by=["EventDate"],
-            properties={"delta.logRetentionDuration": "7 days", "delta.enableChangeDataFeed": "true"},
-            comment="Creation of Delta Table"
-        )
-
-        result = table_create_utility.execute()
-        ```
     """
 
     spark: SparkSession
