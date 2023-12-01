c82288a60eb9a9a7e296f36122b403b8007621d6
rodalynbarce <Rodalyn.Barce@shell.com>
2023-12-01 13:32:14 +0000

    Updates
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>

--- src/sdk/python/rtdip_sdk/integrations/openstef/interfaces.py
+++ src/sdk/python/rtdip_sdk/integrations/openstef/interfaces.py
@@ -27,9 +27,6 @@ from openstef_dbc.ktp_api import KtpApi
 from openstef_dbc.log import logging
 from ._query_builder import _query_builder
 from importlib_metadata import version
-from ..._sdk_utils.compare_versions import (
-    _package_version_meets_minimum,
-)
 
 
 class _DataInterface(_DataInterface, metaclass=Singleton):
@@ -62,8 +59,6 @@ class _DataInterface(_DataInterface, metaclass=Singleton):
                 db_http_path (str): SQL warehouse http path.
                 proxies Union[dict[str, str], None]: Proxies.
         """
-
-        _package_version_meets_minimum("openstef", "3.3.6")
 
         import openstef_dbc.data_interface
 
