b37768fa9f62bec0690855089173b3fad3859c79
Chloe Ching <chloe.ching@shell.com>
2023-12-08 15:54:15 +0000

    fix  bug
    Signed-off-by: Chloe Ching <chloe.ching@shell.com>

--- tests/sdk/python/rtdip_sdk/pipelines/transformers/spark/test_mirico_json_to_pcdm.py
+++ tests/sdk/python/rtdip_sdk/pipelines/transformers/spark/test_mirico_json_to_pcdm.py
@@ -83,8 +83,8 @@ def test_mirico_json_to_pcdm(spark_session: SparkSession):
             assert isinstance(mirico_json_to_pcdm_transformer.libraries(), Libraries)
             assert expected_schema == actual_df.schema
             assert expected_df.collect() == actual_df.collect()
-    except Exception as e:
-        with pytest.raises(Exception(e)):
+    except:
+        with pytest.raises(Exception):
             mirico_json_to_pcdm_transformer = MiricoJsonToPCDMTransformer(
                 data=mirico_df, source_column_name="body"
             )
