15032f4176f4337852d923024532318261fe7d10
Amber-Rigg <amber.l.rigg25@gmail.com>
2023-12-18 15:03:11 +0000

    Update UA to AE
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- src/sdk/python/rtdip_sdk/pipelines/transformers/spark/opc_publisher_opcae_json_to_pcdm.py
+++ src/sdk/python/rtdip_sdk/pipelines/transformers/spark/opc_publisher_opcae_json_to_pcdm.py
@@ -36,7 +36,7 @@ class OPCPublisherOPCAEJsonToPCDMTransformer(TransformerInterface):
     ```python
     from rtdip_sdk.pipelines.transformers import OPCPublisherOPCAEJsonToPCDMTransformer
 
-    opc_publisher_opcua_json_to_pcdm_transformer = OPCPublisherOPCAEJsonToPCDMTransformer(
+    opc_publisher_opcae_json_to_pcdm_transformer = OPCPublisherOPCAEJsonToPCDMTransformer(
         data=df,
         souce_column_name="body",
         timestamp_formats=[
