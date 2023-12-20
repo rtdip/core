2668770e07e8fcbc10383a78746e372fc0e1d655
Amber-Rigg <amber.l.rigg25@gmail.com>
2023-12-20 15:38:47 +0000

    Refactor raw_parameters build
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- src/sdk/python/rtdip_sdk/queries/weather/_weather_query_builder.py
+++ src/sdk/python/rtdip_sdk/queries/weather/_weather_query_builder.py
@@ -32,65 +32,41 @@ def _build_parameters(
     area_type: str,
     table_type: str,
 ) -> dict:
-    if area_type == "grid":
-        raw_parameters = {
-            "table_name": parameters_dict["table_name"],
-            "max_lat": parameters_dict["max_lat"],
-            "max_lon": parameters_dict["max_lon"],
-            "min_lat": parameters_dict["min_lat"],
-            "min_lon": parameters_dict["min_lon"],
-            "source": parameters_dict.get("source", None),
-            "limit": parameters_dict.get("limit", None),
-            "latitude_column": parameters_dict.get("latitude_column", "Latitude"),
-            "longitude_column": parameters_dict.get("longitude_column", "Longitude"),
-            "tagname_column": parameters_dict.get("tagname_column", "TagName"),
-        }
-        if table_type == "raw":
-            raw_parameters["start_date"] = parameters_dict["start_date"]
-            raw_parameters["end_date"] = parameters_dict["end_date"]
-            raw_parameters["forecast_run_start_date"] = parameters_dict[
-                "forecast_run_start_date"
-            ]
-            raw_parameters["forecast_run_end_date"] = parameters_dict[
-                "forecast_run_end_date"
-            ]
-            raw_parameters["timestamp_column"] = parameters_dict.get(
-                "timestamp_column", "EventTime"
-            )
-            raw_parameters["forecast_run_timestamp_column"] = parameters_dict.get(
-                "forecast_run_timestamp_column", "EnqueuedTime"
-            )
-
-            raw_parameters["include_status"] = False
+    raw_parameters = {
+        "table_name": parameters_dict["table_name"],
+        "source": parameters_dict.get("source", None),
+        "limit": parameters_dict.get("limit", None),
+        "latitude_column": parameters_dict.get("latitude_column", "Latitude"),
+        "longitude_column": parameters_dict.get("longitude_column", "Longitude"),
+        "tagname_column": parameters_dict.get("tagname_column", "TagName"),
+    }
 
     if area_type == "point":
-        raw_parameters = {
-            "table_name": parameters_dict["table_name"],
-            "lat": parameters_dict["lat"],
-            "lon": parameters_dict["lon"],
-            "source": parameters_dict.get("source", None),
-            "limit": parameters_dict.get("limit", None),
-            "latitude_column": parameters_dict.get("latitude_column", "Latitude"),
-            "longitude_column": parameters_dict.get("longitude_column", "Longitude"),
-            "tagname_column": parameters_dict.get("tagname_column", "TagName"),
-        }
-        if table_type == "raw":
-            raw_parameters["start_date"] = parameters_dict["start_date"]
-            raw_parameters["end_date"] = parameters_dict["end_date"]
-            raw_parameters["forecast_run_start_date"] = parameters_dict[
-                "forecast_run_start_date"
-            ]
-            raw_parameters["forecast_run_end_date"] = parameters_dict[
-                "forecast_run_end_date"
-            ]
-            raw_parameters["timestamp_column"] = parameters_dict.get(
-                "timestamp_column", "EventTime"
-            )
-            raw_parameters["forecast_run_timestamp_column"] = parameters_dict.get(
-                "forecast_run_timestamp_column", "EnqueuedTime"
-            )
+        raw_parameters["lat"] = parameters_dict["lat"]
+        raw_parameters["lon"] = parameters_dict["lon"]
 
-            raw_parameters["include_status"] = False
+    if area_type == "grid":
+        raw_parameters["max_lat"] = parameters_dict["max_lat"]
+        raw_parameters["max_lon"] = parameters_dict["max_lon"]
+        raw_parameters["min_lat"] = parameters_dict["min_lat"]
+        raw_parameters["min_lon"] = parameters_dict["min_lon"]
+
+    if table_type == "raw":
+        raw_parameters["start_date"] = parameters_dict["start_date"]
+        raw_parameters["end_date"] = parameters_dict["end_date"]
+        raw_parameters["forecast_run_start_date"] = parameters_dict[
+            "forecast_run_start_date"
+        ]
+        raw_parameters["forecast_run_end_date"] = parameters_dict[
+            "forecast_run_end_date"
+        ]
+        raw_parameters["timestamp_column"] = parameters_dict.get(
+            "timestamp_column", "EventTime"
+        )
+        raw_parameters["forecast_run_timestamp_column"] = parameters_dict.get(
+            "forecast_run_timestamp_column", "EnqueuedTime"
+        )
+        raw_parameters["include_status"] = False
 
     return raw_parameters
 
