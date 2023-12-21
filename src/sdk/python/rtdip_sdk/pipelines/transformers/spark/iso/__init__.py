ce848f5e115ff75278927e4cc9808245eb5f6851
Shivam Saxena <shivam.saxena@innowatts.com>
2023-12-21 09:11:41 +0530

    Adding CAISO Raw to Meters Data Model Transformer
    Signed-off-by: Shivam Saxena <shivam.saxena@innowatts.com>

--- src/sdk/python/rtdip_sdk/pipelines/transformers/spark/iso/__init__.py
+++ src/sdk/python/rtdip_sdk/pipelines/transformers/spark/iso/__init__.py
@@ -1,3 +1,4 @@
+from .caiso_to_mdm import CAISOToMDMTransformer
 from .ercot_to_mdm import ERCOTToMDMTransformer
 from .miso_to_mdm import MISOToMDMTransformer
 from .pjm_to_mdm import PJMToMDMTransformer
