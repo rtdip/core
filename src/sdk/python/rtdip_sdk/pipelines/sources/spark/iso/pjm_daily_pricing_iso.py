4f89a9283f05d5e126f394d34e4d392705553ca5
sarjeet kumar <sarjeet.kumar@innowatts.com>
2024-02-20 16:27:38 +0530

    Creating new PR for updating source for pjm_pricing connector

--- src/sdk/python/rtdip_sdk/pipelines/sources/spark/iso/pjm_daily_pricing_iso.py
+++ src/sdk/python/rtdip_sdk/pipelines/sources/spark/iso/pjm_daily_pricing_iso.py
@@ -1,19 +1,6 @@
-# Copyright 2022 RTDIP
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from datetime import datetime
 import logging
+import time
 import pandas as pd
 import numpy as np
 import requests
@@ -21,15 +8,13 @@ from pyspark.sql import SparkSession
 from datetime import timedelta
 from io import BytesIO
 
-
 from ...._pipeline_utils.iso import PJM_PRICING_SCHEMA
-
 from . import BaseISOSource
-
 
 class PJMDailyPricingISOSource(BaseISOSource):
     """
-    The PJM Daily Pricing ISO Source is used to read Real-Time and Day-Ahead hourly data from PJM API. It supports both Real-Time Hourly LMPs and Day-Ahead Hourly LMPs data. Real-Time will return privous 1 day, Day-Ahead will return T + 1 days data.
+    The PJM Daily Pricing ISO Source is used to read Real-Time and Day-Ahead hourly data from PJM API. It supports both Real-Time Hourly LMPs and Day-Ahead Hourly LMPs data. 
+    Real-Time will return data for T - 3 to T days and Day-Ahead will return T - 3 to T + 1 days data.
 
     API:              <a href="https://api.pjm.com/api/v1/">  (must be a valid apy key from PJM)
 
@@ -65,51 +50,49 @@ class PJMDailyPricingISOSource(BaseISOSource):
         self.options: dict = options
         self.load_type: str = self.options.get("load_type", "").strip()
         self.api_key: str = self.options.get("api_key", "").strip()
-        self.days: int = self.options.get("days", 7)
+        self.days: int = self.options.get("days", 3)
 
-    def _fetch_from_url(self, url_suffix: str, start_date: str, end_date: str) -> bytes:
+    def _fetch_paginated_data(self, url_suffix: str, start_date: str, end_date: str) -> bytes:
         """
-        Gets data from external ISO API.
+        Fetches data from the PJM API with pagination support.
 
         Args:
-            url_suffix: String to be used as suffix to iso url.
+            url_suffix: String to be used as suffix to ISO URL.
+            start_date: Start date for the data retrieval.
+            end_date: End date for the data retrieval.
 
         Returns:
             Raw content of the data received.
         """
-
-        url = f"{self.iso_url}{url_suffix}"
         headers = {"Ocp-Apim-Subscription-Key": self.api_key}
-        logging.info(
-            f"Requesting URL - {url}, start_date={start_date}, end_date={end_date}, load_type={self.load_type}"
-        )
-        
-        load_key = "datetime_beginning_ept"
-
-        feed = (
-            "rt_hrl_lmps"
-            if self.load_type != "day_ahead"
-            else "da_hrl_lmps"
-        )
-        
+        items = []
         query = {
             "startRow": "1",
-            load_key: f"{start_date}to{end_date}",
-            "format": "csv",
-            "download": "true",
+            "rowCount": "50000",
+            "datetime_beginning_ept":f"{start_date}to{end_date}"
         }
         query_s = "&".join(["=".join([k, v]) for k, v in query.items()])
-        
-        new_url = f"{url}{feed}?{query_s}"
-        response = requests.get(new_url, headers=headers)
-        code = response.status_code
+        base_url = f"{self.iso_url}{url_suffix}?{query_s}"
 
-        if code != 200:
-            raise requests.HTTPError(
-                f"Unable to access URL `{url}`."
-                f" Received status code {code} with message {response.content}"
-            )
-        return response.content
+        next_page = base_url
+        
+        logging.info(
+            f"Requesting URL - {base_url}, start_date={start_date}, end_date={end_date}, load_type={self.load_type}"
+        )
+
+        while next_page:
+            now = datetime.now()
+            logging.info(f"Timestamp: {now}")
+            response = requests.get(next_page, headers=headers)
+            response.raise_for_status()
+            data = response.json() 
+            logging.info(f"Data for page {next_page}:")
+            items.extend(data['items'])
+            next_page = next((link['href'] for link in data['links'] if link['rel'] == 'next'), None)
+            url_suffix = next_page if next_page else ""
+            time.sleep(10)
+
+        return items
 
     def _pull_data(self) -> pd.DataFrame:
         """
@@ -118,17 +101,25 @@ class PJMDailyPricingISOSource(BaseISOSource):
         Returns:
             Raw form of data.
         """
-        start_date = self.current_date - timedelta(days=7)
+        start_date = self.current_date - timedelta(days=3)
         start_date = start_date.replace(hour=0, minute=0)
         end_date = (start_date + timedelta(days=self.days)).replace(hour=23)
         start_date_str = start_date.strftime(self.query_datetime_format)
         end_date_str = end_date.strftime(self.query_datetime_format)
-        df = pd.read_csv(
-            BytesIO(self._fetch_from_url("", start_date_str, end_date_str))
-        )
-        
-        return df
+                
+        if self.load_type == 'day_ahead':
+            url_suffix = 'da_hrl_lmps'
+        else:
+            url_suffix = 'rt_hrl_lmps'
 
+        
+        data = self._fetch_paginated_data(url_suffix, start_date_str, end_date_str)
+            
+        df = pd.DataFrame(data)
+        logging.info(f"Data fetched successfully: {len(df)} rows")
+            
+        return df 
+        
     def _prepare_data(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         Creates a new date time column and removes null values. Renames columns
@@ -182,16 +173,15 @@ class PJMDailyPricingISOSource(BaseISOSource):
         
         df = df.replace({np.nan: None, "": None})
     
-        df["StartTime"] = pd.to_datetime(df["StartTime"], format="%m/%d/%Y %I:%M:%S %p")
-        
-    
+        df["StartTime"] = pd.to_datetime(df["StartTime"])
         df = df.replace({np.nan: None, "": None})
-
+        
         df.reset_index(inplace=True, drop=True)
         
+        df.to_csv("/home/sarjeet/Desktop/Inno_accounts/New_Data/RT_Data_3Days.csv",index=False)
 
         return df
-
+     
     def _validate_options(self) -> bool:
         """
         Validates the following options:
