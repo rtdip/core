4878fb287e6c5f212f20921501112516d506e7ae
Amber-Rigg <amber.l.rigg25@gmail.com>
2024-02-23 15:44:59 +0000

    add test init
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- tests/sdk/python/rtdip_sdk/monitoring/data_quality/pydeequ/spark/test_pydeequ.py
+++ tests/sdk/python/rtdip_sdk/monitoring/data_quality/pydeequ/spark/test_pydeequ.py
@@ -23,13 +23,38 @@ from src.sdk.python.rtdip_sdk.monitoring._monitoring_utils.models import (
 )
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    TimestampType,
+    IntegerType,
+)
 
 from pydeequ.suggestions import ConstraintSuggestionRunner
 from pydeequ.analyzers import AnalysisRunner, AnalyzerContext
 from pydeequ.profiles import ColumnProfilerRunner
 
-test_df = 
+data2 = [
+    ("James", "", "Smith", "36636", "M", 3000),
+    ("Michael", "Rose", "", "40288", "M", 4000),
+    ("Robert", "", "Williams", "42114", "M", 4000),
+    ("Maria", "Anne", "Jones", "39192", "F", 4000),
+    ("Jen", "Mary", "Brown", "", "F", -1),
+]
+
+schema = StructType(
+    [
+        StructField("firstname", StringType(), True),
+        StructField("middlename", StringType(), True),
+        StructField("lastname", StringType(), True),
+        StructField("id", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("salary", IntegerType(), True),
+    ]
+)
+
+test_df = SparkSession.createDataFrame(data=data2, schema=schema)
 
 
 def test_profiles(spark_session: SparkSession):
