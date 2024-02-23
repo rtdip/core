45cfc3aeb3b4f2a185c3bd63c0e87f026a43612d
sarjeet kumar <sarjeet.kumar@innowatts.com>
2024-02-23 16:54:04 +0530

    update the some doc string for pjm_historical_pricing_iso.py

--- src/sdk/python/rtdip_sdk/pipelines/sources/spark/iso/pjm_historical_pricing_iso.py
+++ src/sdk/python/rtdip_sdk/pipelines/sources/spark/iso/pjm_historical_pricing_iso.py
@@ -13,31 +13,27 @@ from . import BaseISOSource
 
 class PJMHistoricalPricingISOSource(PJMDailyPricingISOSource):
     """
-    The PJM Historical Pricing ISO Source is used to read historical Real-Time and Day-Ahead hourly data from PJM API. It supports both Real-Time Hourly LMPs and Day-Ahead Hourly LMPs data. 
+    The PJM Historical Pricing ISO Source is used to retrieve historical Real-Time and Day-Ahead hourly data from the PJM API.
 
-    API:              <a href="https://api.pjm.com/api/v1/">  (must be a valid apy key from PJM)
+    API: <a href="https://api.pjm.com/api/v1/"> (Requires a valid API key from PJM)
+    Real-Time doc: <a href="https://dataminer2.pjm.com/feed/da_hrl_lmps/definition">
+    Day-Ahead doc: <a href="https://dataminer2.pjm.com/feed/rt_hrl_lmps/definition">
 
-    Real-Time doc:    <a href="https://dataminer2.pjm.com/feed/da_hrl_lmps/definition">
+    The PJM Historical Pricing ISO Source accesses the same PJM endpoints as the daily pricing source but is tailored for retrieving data within a specified historical range defined by the `start_date` and `end_date` attributes.
 
-    Day-Ahead doc:    <a href="https://dataminer2.pjm.com/feed/rt_hrl_lmps/definition">
-    
-    The PJM Historical Pricing ISO Source is used same PJM endpoint, but it is called within a range established by the start_date & end_date attributes for historical data.
-    
     Parameters:
-        spark (SparkSession): Spark Session instance
-        options (dict): A dictionary of ISO Source specific configurations (See Attributes table below)
+        - spark (SparkSession): The Spark Session instance.
+        - options (dict): A dictionary of ISO Source specific configurations.
 
     Attributes:
-        api_key (str): Must be a valid key from PJM, see api url
-        load_type (str): Must be one of `real_time` or `day_ahead`
-        start_date (str): Must be valid start_date for historical data
-        start_date (str): Must be valid end_date for historical data
+        - api_key (str): A valid key from PJM required for authentication.
+        - load_type (str): The type of data to retrieve, either `real_time` or `day_ahead`.
+        - start_date (str): The start date for the historical data range.
+        - end_date (str): The end date for the historical data range.
 
-    Please check the BaseISOSource for available methods.
+    Please refer to the BaseISOSource for available methods and further details.
 
-    BaseISOSource:
-        ::: src.sdk.python.rtdip_sdk.pipelines.sources.spark.iso.pjm_daily_pricing_iso.py
-    """
+    BaseISOSource: ::: src.sdk.python.rtdip_sdk.pipelines.sources.spark.iso.base_iso"""
 
     spark: SparkSession
     options: dict
@@ -54,10 +50,10 @@ class PJMHistoricalPricingISOSource(PJMDailyPricingISOSource):
 
     def _pull_data(self) -> pd.DataFrame:
         """
-        Pulls data from the PJM API and parses the return.
+        Pulls historical pricing data from the PJM API within the specified date range.
 
         Returns:
-            Raw form of data.
+            pd.DataFrame: A DataFrame containing the raw historical pricing data retrieved from the PJM API.
         """
        
         logging.info(f"Historical data requested from {self.start_date} to {self.end_date}")
@@ -80,7 +76,7 @@ class PJMHistoricalPricingISOSource(PJMDailyPricingISOSource):
         return df 
             
     def _validate_options(self) -> bool:
-        super()._validate_options()
+         
         """
         Validates all parameters including the following examples:
             - `start_date` & `end_data` must be in the correct format.
@@ -91,7 +87,7 @@ class PJMHistoricalPricingISOSource(PJMDailyPricingISOSource):
             True if all looks good otherwise raises Exception.
 
         """
-
+        super()._validate_options()
         try:
             start_date = datetime.strptime(self.start_date, self.user_datetime_format)
         except ValueError:
