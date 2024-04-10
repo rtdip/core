1a1a0a51c0a1ba186133e44ef156a186ff8cb6e2
rodalynbarce <Rodalyn.Barce@shell.com>
2024-04-10 12:07:28 +0100

    Update tests for Pydantic upgrade
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>

--- tests/api/v1/test_api_raw.py
+++ tests/api/v1/test_api_raw.py
@@ -84,7 +84,7 @@ async def test_api_raw_get_validation_error(mocker: MockerFixture):
     assert response.status_code == 422
     assert (
         actual
-        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.4/v/missing"}]}'
+        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.6/v/missing"}]}'
     )
 
 
@@ -162,7 +162,7 @@ async def test_api_raw_post_validation_error(mocker: MockerFixture):
     assert response.status_code == 422
     assert (
         actual
-        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.4/v/missing"}]}'
+        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.6/v/missing"}]}'
     )
 
 
