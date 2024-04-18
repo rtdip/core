cb80a133208f776734baeb4bc612966b625a6a8f
rodalynbarce <121169437+rodalynbarce@users.noreply.github.com>
2024-04-18 16:17:32 +0100

    Upgrade pydantic to 2.6.0 (#719)
    * Upgrade Pydantic Package
    
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>
    
    * Update tests for Pydantic upgrade
    
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>
    
    * Upgrade pydantic
    
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>
    
    * Upgrade pydantic
    
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>
    
    ---------
    
    Signed-off-by: rodalynbarce <Rodalyn.Barce@shell.com>

--- tests/api/v1/test_api_resample.py
+++ tests/api/v1/test_api_resample.py
@@ -71,7 +71,7 @@ async def test_api_resample_get_validation_error(mocker: MockerFixture):
     assert response.status_code == 422
     assert (
         actual
-        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.4/v/missing"}]}'
+        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.6/v/missing"}]}'
     )
 
 
@@ -134,7 +134,7 @@ async def test_api_resample_post_validation_error(mocker: MockerFixture):
     assert response.status_code == 422
     assert (
         actual
-        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.4/v/missing"}]}'
+        == '{"detail":[{"type":"missing","loc":["query","start_date"],"msg":"Field required","input":null,"url":"https://errors.pydantic.dev/2.6/v/missing"}]}'
     )
 
 
