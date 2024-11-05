4f2a646384fb40e1851c01c6fcc89ff665dc5ef7
Dominik Hoffmann <dominik151099@outlook.de>
2024-11-05 15:20:00 +0100

    #22: implemented first steps of interval filtering
    Signed-off-by: Dominik Hoffmann <dominik151099@outlook.de>

---
+++ src/sdk/python/rtdip_sdk/pipelines/data_wranglers/spark/data_quality/interval_filtering.py
@@ -0,0 +1,127 @@
+# Copyright 2022 RTDIP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import timedelta
+
+import pandas as pd
+from pyspark.sql import functions as F
+from pyspark.sql import DataFrame, SparkSession
+
+from ...._pipeline_utils.models import Libraries, SystemType
+from ...interfaces import WranglerBaseInterface
+
+class IntervalFiltering(WranglerBaseInterface):
+    """
+       Cleanses a DataFrame by removing rows outside a specified interval window.
+       Example:
+
+
+       Parameters:
+           spark (SparkSession): A SparkSession object.
+           df (DataFrame): PySpark DataFrame to be converted
+           interval (int): The interval length for cleansing.
+            interval_unit (str): 'hours', 'minutes', 'seconds' or 'milliseconds' to specify the unit of the interval.
+       """
+
+    """ Default time stamp column name if not set in the constructor """
+    DEFAULT_TIME_STAMP_COLUMN_NAME: str = "EventTime"
+
+    def __init__(self, spark: SparkSession,  df: DataFrame, interval: int, interval_unit: str, time_stamp_column_name: str = None) -> None:
+        self.spark = spark
+        self.df = df
+        self.interval = interval
+        self.interval_unit = interval_unit
+        if time_stamp_column_name is None:
+            self.time_stamp_column_name = self.DEFAULT_TIME_STAMP_COLUMN_NAME
+        else: self.time_stamp_column_name = time_stamp_column_name
+
+    @staticmethod
+    def system_type():
+        """
+        Attributes:
+            SystemType (Environment): Requires PYSPARK
+        """
+        return SystemType.PYSPARK
+
+    @staticmethod
+    def libraries():
+        libraries = Libraries()
+        return libraries
+
+    @staticmethod
+    def settings() -> dict:
+        return {}
+
+
+    def convert_column_to_timestamp(self) -> DataFrame:
+        try:
+            return self.df.withColumn(self.time_stamp_column_name, F.to_timestamp(self.time_stamp_column_name))
+        except Exception as e:
+            raise ValueError(f"Error converting column {self.time_stamp_column_name} to timestamp: {e}")
+
+    def get_time_delta(self) -> timedelta:
+        if self.interval_unit ==  'minutes':
+            return timedelta(minutes = self.interval)
+        elif self.interval_unit == 'days':
+            return timedelta(days = self.interval)
+        elif self.interval_unit == 'hours':
+            return timedelta(hours = self.interval)
+        elif self.interval_unit == 'seconds':
+            return timedelta(seconds = self.interval)
+        elif self.interval_unit == 'milliseconds':
+            return timedelta(milliseconds = self.interval)
+        else:
+            raise ValueError("interval_unit must be either 'seconds' or 'milliseconds'")
+
+    def filter(self) -> DataFrame:
+        """
+        Filters the DataFrame based on the interval
+        """
+
+        if self.time_stamp_column_name not in self.df.columns:
+            raise ValueError(f"Column {self.time_stamp_column_name} not found in the DataFrame.")
+
+        self.df = self.convert_column_to_timestamp()
+
+        time_delta = self.get_time_delta()
+
+        rows = self.df.collect()
+        cleansed_df = [rows[0]]
+
+
+
+        last_time_stamp = rows[0][self.time_stamp_column_name]
+
+
+        for i in range(1, len(rows)):
+            current_row = rows[i]
+            current_time_stamp = current_row[self.time_stamp_column_name]
+
+            if ((last_time_stamp - last_time_stamp).total_seconds()) >= time_delta.total_seconds():
+                print(current_row)
+                cleansed_df.append(current_row)
+                last_time_stamp = current_time_stamp
+
+        print(cleansed_df)
+
+
+        return self.df
+
+
+
+
+
+
+
+
+
---
+++ tests/sdk/python/rtdip_sdk/pipelines/data_wranglers/spark/data_quality/test_interval_filtering.py
@@ -0,0 +1,63 @@
+# Copyright 2022 RTDIP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+
+from rtdip_sdk.pipelines.data_wranglers.spark.data_quality.interval_filtering import IntervalFiltering
+from src.sdk.python.rtdip_sdk.pipelines.data_wranglers.spark.data_quality.duplicate_detection import (
+    DuplicateDetection,
+)
+
+
+@pytest.fixture(scope="session")
+def spark_session():
+    return SparkSession.builder.master("local[2]").appName("test").getOrCreate()
+
+
+def test_interval_detection(spark_session: SparkSession):
+    expected_df = spark_session.createDataFrame(
+        [
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 20:03:46.000", "Good", "0.340000004"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 16:00:12.000", "Good", "0.150000006"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 11:56:42.000", "Good", "0.129999995"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 07:53:11.000", "Good", "0.119999997"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 03:49:45.000", "Good", "0.129999995"),
+        ],
+        ["TagName", "EventTime", "Status", "Value"],
+    )
+
+    df = spark_session.createDataFrame(
+        [
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 20:03:46.000", "Good", "0.340000004"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 20:03:46.000", "Good", "0.340000004"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 16:00:12.000", "Good", "0.150000006"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 11:56:42.000", "Good", "0.129999995"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 07:53:11.000", "Good", "0.119999997"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 07:53:11.000", "Good", "0.119999997"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 07:53:11.000", "Good", "0.119999997"),
+            ("A2PS64V0J.:ZUX09R", "2024-01-02 03:49:45.000", "Good", "0.129999995"),
+        ],
+        ["TagName", "EventTime", "Status", "Value"],
+    )
+
+    interval_filtering_wrangler = IntervalFiltering(spark_session, df, 1, "milliseconds")
+    actual_df = interval_filtering_wrangler.filter()
+
+    assert isinstance(actual_df, DataFrame)
+
+    assert expected_df.columns == actual_df.columns
+    assert expected_df.schema == actual_df.schema
+    assert expected_df.collect() == actual_df.collect()
