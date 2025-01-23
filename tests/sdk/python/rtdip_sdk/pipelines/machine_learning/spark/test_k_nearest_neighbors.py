11c6db92bd4ffd70d189618409e9d4a63a920a1d
Minh Khue Tran <minh.khue.tran@fau.de>
2025-01-23 15:07:38 +0100

    linted
    Signed-off-by: Minh Khue Tran <minh.khue.tran@fau.de>

--- tests/sdk/python/rtdip_sdk/pipelines/machine_learning/spark/test_k_nearest_neighbors.py
+++ tests/sdk/python/rtdip_sdk/pipelines/machine_learning/spark/test_k_nearest_neighbors.py
@@ -22,7 +22,9 @@ from pyspark.sql.types import (
     FloatType,
 )
 from datetime import datetime
-from src.sdk.python.rtdip_sdk.pipelines.machine_learning.spark.k_nearest_neighbors import KNearestNeighbors
+from src.sdk.python.rtdip_sdk.pipelines.machine_learning.spark.k_nearest_neighbors import (
+    KNearestNeighbors,
+)
 from pyspark.ml.feature import VectorAssembler, StandardScaler, StringIndexer
 from pyspark.sql.functions import col
 
