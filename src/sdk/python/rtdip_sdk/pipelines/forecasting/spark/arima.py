919d603ee095ff5a980dfacdf501b18308cbdd21
Timm638 <Timm638@gmail.com>
2025-01-27 13:51:48 +0100

    fixes missing preparation from pandas DF to spark DF
    Signed-off-by: Timm638 <Timm638@gmail.com>

--- src/sdk/python/rtdip_sdk/pipelines/forecasting/spark/arima.py
+++ src/sdk/python/rtdip_sdk/pipelines/forecasting/spark/arima.py
@@ -424,7 +424,7 @@ class ArimaPrediction(DataManipulationBaseInterface, InputValidator):
             data_to_add = _prepare_pandas_to_convert_to_spark(data_to_add)
 
             predicted_source_pyspark_dataframe = self.spark_session.createDataFrame(
-                data_to_add[[self.source_name, self.timestamp_name, self.value_name]],
+                _prepare_pandas_to_convert_to_spark(data_to_add[[self.source_name, self.timestamp_name, self.value_name]]),
                 schema=pd_df_schema,
             )
 
