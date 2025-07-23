f75353c6dc743fc53425685f27da9cfa6ad54bf3
cching95 <chloe.ching@shell.com>
2025-07-23 11:27:33 +0100

    test fix
    Signed-off-by: cching95 <chloe.ching@shell.com>

--- tests/sdk/python/rtdip_sdk/pipelines/deploy/test_databricks_deploy.py
+++ tests/sdk/python/rtdip_sdk/pipelines/deploy/test_databricks_deploy.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+# import sys
 
-sys.path.insert(0, ".")
+# sys.path.insert(0, ".")
 from pytest_mock import MockerFixture
 import pytest
 
