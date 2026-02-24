64f8a8967d342284074229ddf0e5373357d5f27a
Amber-Rigg <amber.l.rigg25@gmail.com>
2026-02-24 14:25:00 +0000

    Update requirements and refactor SparkConnection import handling for improved compatibility
    Signed-off-by: Amber-Rigg <amber.l.rigg25@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,14 +15,7 @@ import pytest
 import os
 import shutil
 
-try:
-    from src.sdk.python.rtdip_sdk.connectors.grpc.spark_connector import (
-        SparkConnection,
-    )
-except (ImportError, ModuleNotFoundError):
-    # SparkConnection import may fail if turbodbc dependencies are missing
-    SparkConnection = None
-
+from src.sdk.python.rtdip_sdk.connectors.grpc.spark_connector import SparkConnection
 from src.sdk.python.rtdip_sdk.pipelines.destinations import *  # NOSONAR
 from src.sdk.python.rtdip_sdk.pipelines.sources import *  # NOSONAR
 from src.sdk.python.rtdip_sdk.pipelines.utilities.spark.session import (
@@ -56,9 +49,7 @@ def spark_session():
 
 
 @pytest.fixture(scope="session")
-def spark_connection(spark_session):
-    if SparkConnection is None:
-        pytest.skip("SparkConnection unavailable - turbodbc dependencies missing")
+def spark_connection(spark_session: SparkSession):
     table_name = "test_table"
     data = [
         {
